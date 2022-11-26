c54031feef377df20a00b9df796d350ff58bbaf7
Alex Rahalevich <alex.rahalevich@dr-bill.ca>
2022-11-25 19:42:09 -0800

    formatting

--- lua_init.lua
+++ lua_init.lua
@@ -1,13 +1,13 @@
 --  LSP configuration
 --
-local cmp = require'cmp'
+local cmp = require('cmp')
 
-local lspconfig = require'lspconfig'
+local lspconfig = require('lspconfig')
 cmp.setup({
   snippet = {
     -- REQUIRED by nvim-cmp. get rid of it once we can
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
+      vim.fn['vsnip#anonymous'](args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
@@ -60,19 +60,19 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
   -- Get signatures (and _only_ signatures) when in argument lists.
-  require "lsp_signature".on_attach({
+  require 'lsp_signature'.on_attach({
     doc_lines = 0,
     handler_opts = {
-      border = "none"
+      border = 'none'
     },
   })
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local util = require 'lspconfig.util'
+local util = require('lspconfig.util')
 
 lspconfig.solargraph.setup {
   init_options = { formatting = true },
@@ -86,7 +86,7 @@ lspconfig.rust_analyzer.setup {
     debounce_text_changes = 150,
   },
   settings = {
-    ["rust-analyzer"] = {
+    ['rust-analyzer'] = {
       cargo = {
         allFeatures = true,
       },
@@ -100,12 +100,12 @@ lspconfig.rust_analyzer.setup {
   capabilities = capabilities,
 }
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-  }
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = true,
+  signs = true,
+  update_in_insert = true,
+}
 )
 
 require('gitsigns').setup {
