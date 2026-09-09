295f1b2fa34cabf93e29cb7b0355fe6d3a696eac
saksmlz <saksmlz@gmail.com>
2026-09-09 10:20:45 -0700

    Revert "more stable highlights"
    This reverts commit 1848d3418c20d23add920e38a4040446d2eb42e7.

--- queries/rust/highlights.scm
+++
@@ -1,7 +0,0 @@
-; extends
-
-; `use { foo, Bar }` — lowercase names are functions, not plain variables.
-(use_list
-  (identifier) @function
-  (#lua-match? @function "^[a-z]")
-  (#set! priority 110))
